15e7f4c4c46cd4e7ac217861363f0eb6f91e9f3a
Shrihari <ext_shriharir@Falabella.cl>
2021-04-30 17:42:32 +0530

    [no id] | Shrihari | Gradle project setup

---
+++ src/main/java/com/help/covid/covidassistindiabackend/CovidAssistIndiaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.help.covid.covidassistindiabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidAssistIndiaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidAssistIndiaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/help/covid/covidassistindiabackend/CovidAssistIndiaBackendApplicationTests.java
@@ -0,0 +1,8 @@
+package com.help.covid.covidassistindiabackend;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidAssistIndiaBackendApplicationTests {
+
+}
